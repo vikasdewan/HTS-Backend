7547879d14ce26c3b139f35ec7f3eb08b184ee70
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-09-19 21:54:58 +0530

    Utilities such as apierror , apiresponse , asynchandler are added

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse {
+    constructor(statusCode, data , message = "Success"){
+        this.statusCode = statusCode
+        this.data = data 
+        this.message = message
+        this.success = this.statusCode < 400
+    }
+}
+
+export {ApiResponse}
