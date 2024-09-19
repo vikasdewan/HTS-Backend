266022cbd7bae32434feb23d9babc9365562e8b1
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-09-19 21:56:21 +0530

    multer is used for file upload

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer  from "multer";
+//cb -> callback
+const storage = multer.diskStorage({
+    destination: function (req,file,cb) {
+        cb(null,"./public/temp")
+    },
+    filename: function(req,file,cb){
+        cb(null,file.originalname)
+    }
+})
+
+export const upload = multer({
+    storage,
+})
