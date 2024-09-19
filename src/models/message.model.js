9699129c7a8124f8625dd11cb2a197970d63f89f
adityach0uhan <m4with6x@gmail.com>
2024-09-19 22:16:31 +0530

    Added message model

---
+++ src/models/message.model.js
@@ -0,0 +1,38 @@
+// {
+//   messageId: String,
+//   productId: String,
+//   senderId: String,
+//   receiverId: String,
+//   message: String,
+//   timestamp: Date
+// }
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    messageId: {
+      type: String,
+      required: true,
+    },
+    productId: {
+      type: String,
+      required: true,
+    },
+    senderId: {
+      type: String,
+      required: true,
+    },
+    receiverId: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const MessageModel = mongoose.model("Message Details", messageSchema);
+export default MessageModel;
