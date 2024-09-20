baa35cc41700b0a70c572d758bc159806905503c
adityach0uhan <m4with6x@gmail.com>
2024-09-20 23:07:09 +0530

    more changes

--- src/routes/campus-connect.route.js
+++ src/routes/campus-connect.route.js
@@ -4,10 +4,18 @@ const router = express.Router();
 
 router.get("/user", getUserInformation);
 router.post("/user", createUser);
+router.put("/user", updateUser);
+router.delete("/user", deleteUser);
+
 router.get("/notification", getAllNotification);
 router.get("/discussion", getDiscussionController);
+
 router.get("/event", getEventController);
 router.get("/friend", getFriendController);
+router.delete("/friend:id", deleteFriendController);
+
 router.get("/message", getMessageController);
+router.post("/message", postMessageController);
+router.delete("/message:id", deleteMessageController);
 
 module.exports = router;
