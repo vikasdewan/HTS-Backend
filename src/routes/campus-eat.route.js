cca6e1a4ff1182aaad0bc1f166b37b3dfceeb054
adityach0uhan <m4with6x@gmail.com>
2024-09-20 22:52:09 +0530

    created events route

--- src/routes/campus-eat.route.js
+++ src/routes/campus-eat.route.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// app.use("/api/v1/campus-eat/menu", menuRouter);
-// app.use("/api/v1/campus-eat/preorder", preorderRouter);
-// app.use("/api/v1/campus-eat/review", reviewRouter);
-
 router.get("/menu", getMenuController);
 router.get("/preorder", getPreorderController);
 router.get("/review", getReviewController);
