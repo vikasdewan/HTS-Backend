35c6bc4c89297aa4db12a667e65dcb312a718966
adityach0uhan <m4with6x@gmail.com>
2024-09-22 14:01:47 +0530

    added product route and controller

--- src/routes/campus-store.route.js
+++ src/routes/campus-store.route.js
@@ -1,9 +1,18 @@
+import {
+  getAllProducts,
+  deleteProductById,
+  updateProductById,
+  getProductById,
+  addNewProduct,
+} from "../controllers/campus-store/product.controller";
+
 import { Router } from "express";
 const router = Router();
 
-router.get("/product", getProductController);
-router.get("/payment", getPaymentController);
-router.get("/message", getMessageController);
-router.get("/favourite", getFavouriteController);
+router.get("/product", getAllProducts);
+router.post("/product", addNewProduct);
+router.get("/product/:id", getProductById);
+router.put("/product/:id", updateProductById);
+router.delete("/product/:id", deleteProductById);
 
 module.exports = router;
