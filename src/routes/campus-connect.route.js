267bf6f1f25d3b1fcdd7af52f2dce38ab08ac70e
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-09-22 21:48:08 +0530

    user routes completed

--- src/routes/campus-connect.route.js
+++ src/routes/campus-connect.route.js
@@ -1,14 +1,27 @@
 import express from "express";
 const router = express.Router();
+import { upload } from "../middlewares/multer.middleware.js";
+import {verifyJWT} from '../middlewares/isLogin.middleware.js'
 import {
   registerUser,
   loginUser,
+  logoutUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateProfileImage,
 } from "../controllers/campus-connect/user.controllers.js";
-import { upload } from "../middlewares/multer.middleware.js";
 
+//Routes for User 
 router
   .route("/user/register")
   .post(upload.single("profileImage"), registerUser);
 router.route("/user/login").post(loginUser);
+//secured routes (authentication required)
+router.route("/user/logout").get(verifyJWT,logoutUser);
+router.route("/user/change-password").post(verifyJWT,changeCurrentPassword);
+router.route("/user/current-user").get(verifyJWT,getCurrentUser);
+router.route("/user/update-account").patch(verifyJWT,updateAccountDetails);
+router.route("/user/update/profileImage").patch(verifyJWT,upload.single("profileImage"),updateProfileImage);
 
 export default router;
